a47521067e319e975bf963816571c250119c0387
João Gabriel <main.joaogabrielsg@gmail.com>
2025-02-08 17:19:45 -0300

    chore: remove t3-oss/env-nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import createJiti from "jiti";
 import createNextIntlPlugin from "next-intl/plugin";
-import { fileURLToPath } from "node:url";
-
-const jiti = createJiti(fileURLToPath(import.meta.url));
-jiti("./src/env/server");
-jiti("./src/env/client");
 
 const withNextIntl = createNextIntlPlugin();
 
