b7956d7213b857a3b79054720f04609c28addde7
João Gabriel <main.joaogabrielsg@gmail.com>
2025-05-05 10:27:54 -0300

    chore: change next config file extension

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+const withNextIntl = createNextIntlPlugin();
+
+export default withNextIntl(nextConfig);
